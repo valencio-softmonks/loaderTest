253d911c8300231f05a591bf6b4f7357b0697d61
valencio-softmonks <valencio@softmonks.com>
2021-05-29 20:20:04 +0530

    Initial Commit

--- app/src/main/java/com/valencio/squareloader/MainActivity.kt
+++ app/src/main/java/com/valencio/squareloader/MainActivity.kt
@@ -7,15 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        /*val view = findViewById(R.id.view) as SlidingSquareLoaderView
-
-        findViewById(R.id.button1).setOnClickListener {
-            view.show()
-        }
-
-        findViewById(R.id.button2).setOnClickListener {
-            view.hide()
-        }*/
+        /*  val loaderView = findViewById<SlidingSquareLoaderView>(R.id.loader123)
+            loaderView.show()*/
     }
 }
